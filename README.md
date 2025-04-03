7e06efd854b0b6cfd522d7e4ca8038b97e6a5797
Toast <the.zero.toaster@gmail.com>
2025-04-03 13:31:37 -0400

    initial project setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mamaki Maker
+
+A Minecraft mod adding a certain cat maid as an adoptable companion.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for detai
---
+++ src/main/java/net/ztoast/mamaki/MamakiMakerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ztoast.mamaki;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MamakiMakerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
